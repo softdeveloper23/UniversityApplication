b2937439641205be61ea31994df333468cac5d8b
softdeveloper23 <brannongarrett77@gmail.com>
2024-06-07 17:02:30 +0200

    Added println method

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -1,7 +1,25 @@
 package main;
 
+import university.Course;
+import university.Student;
+import university.Enrollment;
+
 public class main {
     public static void main(String[] args) {
+        Student student = new Student();
+        student.studentId = 1;
+        student.name ="John Davis";
 
+        Course course = new Course();
+        course.courseId = 102;
+        course.courseName = "Java Programming";
+
+        Enrollment enrollment = new Enrollment();
+        enrollment.student = student;
+        enrollment.course = course;
+
+        System.out.println("Student ID: " + student.studentId + ", Student Name: " + student.name);
+        System.out.println("Course ID: " + course.courseId + ", Course Name: " + course.courseName);
+        System.out.println("Enrolled Student: " + enrollment.student.name + ", Enrolled Course: " + enrollment.course.courseName);
     }
 }
